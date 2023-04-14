9a68a715d9c14990ae27469a5b309977e5b5509f
Hautemaniere <edouard.hte@gmain.com>
2023-04-14 15:29:37 +0200

    Fin pack

--- connexion2/Pack.php
+++ connexion2/Pack.php
@@ -1,28 +1,35 @@
 <?php
-class Pack{
+class Pack
+{
 
     private $id;
     private $Marque;
     private $Model;
 
-    public function setId($id){
+    public function setId($id)
+    {
         $this->id = $id;
     }
-    public function getId($id){
+    public function getId($id)
+    {
         return $this->id;
     }
 
-    public function setMarque($Marque){
+    public function setMarque($Marque)
+    {
         $this->Marque = $Marque;
     }
-    public function getMarque($Marque){
+    public function getMarque($Marque)
+    {
         return $this->Marque;
     }
 
-    public function setModel($Model){
+    public function setModel($Model)
+    {
         $this->Model = $Model;
     }
-    public function getModel($Model){
+    public function getModel($Model)
+    {
         return $this->Model;
     }
 
@@ -33,42 +40,50 @@ class Pack{
         $this->Model = $Model;
     }
 
-    public function getAllPack(){
+    public function getAllPack()
+    {
 
         $lesPacks = array();
-        $req = 'SELECT * FROM `pcloc` ' ;
+        $req = 'SELECT * FROM `pcloc` ';
 
         $resultat = $GLOBALS['bdd']->query($req);
-        while($tab = $resultat->fetch)
-        {
-        $pack = new Pack($tab['id'],$tab['Marque'],$tab['Model']);
-        array_push($lesPacks,$pack);
+        while ($tab = $resultat->fetch) {
+            $pack = new Pack($tab['id'], $tab['Marque'], $tab['Model']);
+            array_push($lesPacks, $pack);
         }
         return $lesPacks;
     }
 
-    public function getPackById($id){
-
-
+    public function getPackById($id)
+    {
 
         //$tabfect = reponse->fetch()
         //$this->marque = $tabfect["marque"];
 
-        return $this;
-    }
+        $lesPacks = array();
+        $req = 'SELECT id FROM `pcloc` ';
 
+        $resultat = $GLOBALS['bdd']->query($req);
+        if ($resultat->rowCount() > 0){
+            $tab = $resultat->fetch();
+            $this->id = $tab['id'];
 
-    public function newPack($nmarque,$model){
-
-        $requete = $GLOBALS['bdd']->prepare('INSERT INTO `pcloc` (`Marque`, `Model`) VALUES ("'.$_POST["nom"].'","'.$_POST.'")');
-        $requete->execute(array($this->Marque, $this->Model));
-
-        if ($requete->rowCount()>0){
-
-            return true;
-        }else{
-            return false;
+        }
+            
         }
 
+
+    public function newPack($nmarque, $model)
+    {
+
+        $requete ='INSERT INTO `pcloc` (`Marque`, `Model`) VALUES ("' . $_POST["Marque"] . '","' . $_POST["Model"] . '")';
+        echo $requete;
+        $requete = $GLOBALS['pdo']->query($requete);
+        if ($requete->rowCount() > 0) {
+
+            return $GLOBALS['pdo']->lastInsertID();;
+        } else {
+            return 0;
+        }
     }
-    }
+}
